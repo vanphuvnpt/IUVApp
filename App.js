5d890829a49ba163c970d0b5df20c4396033ef26
unknown <vanphuvnpt@gmail.com>
2022-04-18 21:57:42 +0700

    1

---
+++ App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Provider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { theme } from "./src/core/theme";
+
+import {
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+  Dashboard,
+} from "./src/screens";
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="StartScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen
+            name="ResetPasswordScreen"
+            component={ResetPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ README.md
@@ -0,0 +1,15 @@
+1. Install dependencies
+   a. format code
+   yarn add --dev --exact prettier
+
+   b. navigation
+
+- yarn add @react-navigation/native
+- expo install react-native-screens react-native-safe-area-context
+  -yarn add @react-navigation/native-stack
+- yarn add @react-navigation/drawer
+- yarn add @react-native-async-storage/async-storage
+- yarn add @react-native-community/masked-view
+  -yarn add react-native-svg react-native-vector-icons
+- yarn add @react-navigation/material-bottom-tabs react-native-paper react-native-vector-icons
+  -expo install react-native-gesture-handler react-native-reanimated
